93d80f0afa944c2dcc1cef826f33f3286388715e
madvolume <gleb.generalyuk@ed-era.com>
2015-05-09 23:24:48 +0300

    123

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
   $("a.toggle-search").remove();
 });
+$(document).ready(function() {
+	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+});
