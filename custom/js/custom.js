53022cc84d6e04200f96434353e7d7a12fbf1488
madvolume <gleb.generalyuk@ed-era.com>
2015-05-09 23:36:19 +0300

    123

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -1,13 +1,4 @@
 $(document).ready(function () {
   $("a.toggle-search").remove();
-  
-  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-		disableOn: 700,
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
-
-		fixedContentPos: false
 });
 
