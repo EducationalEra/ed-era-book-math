0feebf02ff3805aba85a5b70e74bfe17c5d38cf9
artemilchuk <artem.ilchuk@ed-era.com>
2017-09-25 13:45:20 +0300

    added set interval that waits for mathjax

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -1,18 +1,33 @@
 $(document).ready(function () {
-  function init () {
+  function init() {
     var href, $lastLink;
 
     $("a.toggle-search").remove();
 
     $lastLink = $(".custom-link:last");
     href = $lastLink.prop("href");
-    href = "https://ed-era.com/books/" + "maths";
+    href = "https://ed-era.com/books/" + _.last(href.split("-"));
     $lastLink.prop("href", href);
   }
 
   init();
   require(["gitbook"], function (gitbook) {
+    function startMathJax() {
+      MathJax.Hub.Config({
+        tex2jax: {}
+      });
+
+      gitbook.events.bind("page.change", function () {
+        MathJax.Hub.Typeset();
+      });
+    }
     gitbook.events.bind("page.change", init);
+    var interval = null
+    interval = setInterval(function () {
+      if (typeof MathJax === "object") {
+        startMathJax()
+        clearInterval(interval);
+      }
+    }, 1000);
   });
 });
-
